df2d07a0b3e7103bd8aa4549245cf1c2d837e8f3
Manojpun414 <121884004+Manojpun414@users.noreply.github.com>
2023-06-17 09:52:10 +0545

    Add files via upload

---
+++ rproject2.js
@@ -0,0 +1,90 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+    wins : 0,
+    loses : 0,
+    ties : 0,
+}
+ 
+updatescoreElement();
+
+function playGame(playerMove){
+    const computerMove = pickComputerMove();
+     
+    let result = '';
+    
+    if (playerMove === "Scissor"){
+        if (computerMove === "Rock"){
+            result = "You lose";
+        }
+        else if (computerMove === "Paper"){
+            result = "You win";
+        }
+        else if (computerMove === "Scissor"){
+            result = "Tie";
+        }
+    }
+    else if (playerMove === "Paper"){
+        if (computerMove === "Rock"){
+            result = "You win";
+        }
+        else if (computerMove === "Paper"){
+            result = "Tie";
+        }
+        else if (computerMove === "Scissor"){
+            result = "You lose";
+        }
+    }     
+    else if (playerMove === "Rock"){
+        if (computerMove === "Rock"){
+            result = "Tie";
+        }
+        else if (computerMove === "Paper"){
+            result = "You Lose";
+        }
+        else if (computerMove === "Scissor"){
+            result = "You win";
+        }
+    }   
+
+    if (result === "You win"){
+        score.wins += 1;
+    }
+    else if (result === "You lose"){
+        score.loses += 1;
+    }
+    else if (result === "Tie"){
+        score.ties += 1;
+    }
+
+    localStorage.setItem("score",JSON.stringify(score)); /*localStorage only supports string but after comma score is a JS object so we use JSON.stringify to convert it into string*/
+
+    updatescoreElement();
+
+    document.querySelector(".js-result").innerHTML = result;
+
+    document.querySelector(".js-moves").innerHTML = `You
+<img src="./images/${playerMove}-emoji.png" class="move-icon">
+Computer
+<img src="./images/${computerMove}-emoji.png" class="move-icon">`;
+
+}
+
+function updatescoreElement(){
+    document.querySelector(".js-score").innerHTML = `Wins : ${score.wins}, Loses : ${score.loses}, Ties : ${score.ties}`;
+}
+
+function pickComputerMove(){
+    const randomNumber = Math.random();
+    let computerMove  = '';
+    if (randomNumber  >=0 && randomNumber <1/3){
+        computerMove ='Rock';
+    }
+    else if (randomNumber  >=1/3 && randomNumber <2/3){
+        computerMove ='Paper';
+    }
+    else if (randomNumber >=2/3 && randomNumber  <1){
+        computerMove ='Scissor';
+    }
+
+    return computerMove;
+
+}
